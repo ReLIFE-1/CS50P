f9418672ae62903cd7f3c2c8680a6acd6c831451
Relife <yi11a1917vh@gmail.com>
2024-07-18 22:47:51 +0800

    WEEK 0 和 WEEK 1
    添加了第一节课和第二节课的习题解答，不保证正确

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CS50P
 CS50P Problem Set Solution
+暑假自学Python，CS50P 课后习题
+
---
+++ WEEK 1/deep.py
@@ -0,0 +1,6 @@
+x = input("What is the Anwer to the Great Question of the Life, the Universe, and Everyting? ").lower().strip() #去除首尾空格
+
+if x == "42" or x == "forty-two" or x == "forty two" :
+    print("Yes")
+else :
+    print("No")
